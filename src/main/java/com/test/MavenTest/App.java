5db30614a1a938bdc246075ee98e569b4498de40
Administrator <737183245@qq.com>
2016-09-07 10:33:11 +0800

    通天塔

--- src/main/java/com/test/MavenTest/App.java
+++ src/main/java/com/test/MavenTest/App.java
@@ -12,7 +12,6 @@ public class App
     public static void main( String[] args )
     {
     	Random a=new Random();
-    	
     	Date b=new Date();
     	System.out.println(b.toLocaleString());
     	System.out.println(a.nextInt());
